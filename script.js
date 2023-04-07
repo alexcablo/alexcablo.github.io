75766a2eab626b0f4523f8fc3b3aa1dc33bb704f
alexcablo <alexcablo@hotmail.com>
2023-04-07 08:39:52 +0000

    First commit

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarTexto() {
+    var entradaTexto = document.getElementById("entradaTexto").value;
+    document.getElementById("resultado").innerHTML = "Texto ingresado: " + entradaTexto;
+}
